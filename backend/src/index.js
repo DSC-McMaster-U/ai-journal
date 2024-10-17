f1ccaabdd44dcbf69ac5aeeb8df3a196b72bc434
matthewferr <ferrem15@mcmaster.ca>
2024-10-17 16:38:25 -0400

    added swagger to express backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
+const { specs, swaggerUi } = require("./swagger");
+
 const app = express();
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
