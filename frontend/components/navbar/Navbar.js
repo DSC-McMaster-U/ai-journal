ef7b03c5ed92d8ed9ba9a222894aecc8913ee8a7
Andy Pak <im.andy.pak@gmail.com>
2024-10-18 20:53:50 -0400

    Add a bit of style to navbar

--- frontend/components/navbar/Navbar.js
+++ frontend/components/navbar/Navbar.js
@@ -1,3 +1,6 @@
+'use client';
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -24,13 +27,18 @@ export default function Navbar() {
         },
     ];
 
+    const currentPath = usePathname();
+
     // PLACEHOLDER
     const isLoggedIn = true;
 
     return (
-      <div className="flex justify-evenly">
+      <div className="flex justify-evenly items-center bg-indigo-100 h-24">
         {routes.map((route) => (
-            <Link href={isLoggedIn ? route.path : '/login'} key={route.name}>{route.name}</Link>
+            <Link className={
+                `${currentPath === route.path ? 'text-purple-600' : 'text-gray-600'}
+                transition ease-in-out duration-500`
+            } href={isLoggedIn ? route.path : '/login'} key={route.name}>{route.name}</Link>
         ))}
       </div>
     );
