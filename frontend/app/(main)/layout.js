0315844fccec0b9c333df454f9f8d01d01d5b1d0
Andy Pak <im.andy.pak@gmail.com>
2024-10-18 21:09:31 -0400

    Add Font Awesome icons for navbar

--- frontend/app/(main)/layout.js
+++ frontend/app/(main)/layout.js
@@ -1,4 +1,7 @@
 import Navbar from '@/components/navbar/Navbar'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 export default function MainLayout({ children }) {
   return (
--- frontend/components/navbar/Navbar.js
+++ frontend/components/navbar/Navbar.js
@@ -2,28 +2,35 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBook, faComments, faHouse, faChartColumn, faGear } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
     const routes = [
         {
             name: 'Journals',
             path: '/journals',
+            icon: faBook,
         },
         {
             name: 'Chats',
             path: '/chats',
+            icon: faComments,
         },
         {
             name: 'Dashboard',
             path: '/dashboard',
+            icon: faHouse,
         },
         {
             name: 'Stats',
             path: '/stats',
+            icon: faChartColumn,
         },
         {
             name: 'Settings',
             path: '/settings',
+            icon: faGear,
         },
     ];
 
@@ -37,8 +44,8 @@ export default function Navbar() {
         {routes.map((route) => (
             <Link className={
                 `${currentPath === route.path ? 'text-purple-600' : 'text-gray-600'}
-                transition ease-in-out duration-500`
-            } href={isLoggedIn ? route.path : '/login'} key={route.name}>{route.name}</Link>
+                transition ease-in-out duration-500 text-3xl`
+            } href={isLoggedIn ? route.path : '/login'} key={route.name}><FontAwesomeIcon icon={route.icon}/></Link>
         ))}
       </div>
     );
