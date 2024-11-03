96120b03cb5c919b47ef57b18db51d598db80c2c
Austin Bray <93416177+AustinBray77@users.noreply.github.com>
2024-11-03 18:37:51 -0500

    Updated login page to check if user exists in backend session (if login method is passed)

--- frontend/app/(auth)/login/page.js
+++ frontend/app/(auth)/login/page.js
@@ -1,4 +1,21 @@
-export default function LoginPage() {
+"use client";
+
+import { redirect, useSearchParams } from "next/navigation";
+
+export default async function LoginPage() {
+    const params = useSearchParams();
+
+    if (params.get("method") == "o-auth-google") {
+        fetch("http://localhost:3001/api/users/get-session-user")
+            .catch((err) => {
+                alert("No user found. " + err);
+            })
+            .then((res) => {
+                alert("User id: " + res["id"]);
+                redirect("/dashboard");
+            });
+    }
+
     return <div>LoginPage: {GoogleOAuthButton()}</div>;
 }
 
