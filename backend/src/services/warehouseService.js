d0add585d7ea39b32d127050ccb70829411a42ad
abedmohammed <abedmohammed353@gmail.com>
2024-11-11 07:35:15 -0500

    Initializing mysql DB connection

---
+++ backend/src/services/warehouseService.js
@@ -0,0 +1,40 @@
+// services/warehouseService.js
+const connection = require("../database");
+
+// Fetch all warehouses
+const getAllWarehouses = () => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM `ai-journal`.`warehouses`",
+      (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
+};
+
+// Fetch warehouse by ID
+const getWarehouseById = (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM `ai-journal`.`warehouses` WHERE id = ?",
+      [id],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
+};
+
+module.exports = {
+  getAllWarehouses,
+  getWarehouseById,
+};
