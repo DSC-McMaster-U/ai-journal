ad56730bfafb7bb5df9e02b80f1ae88a7af51c64
Angela <angi.sf7@gmail.com>
2024-11-14 19:41:37 -0500

    wrote sql query in services

---
+++ backend/src/services/daily_recordsService.js
@@ -0,0 +1,40 @@
+// services/daily_recordsService.js
+const connection = require("../database");
+
+// Fetch all daily records
+const getAllDaily_Records = () => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM `ai-journal`.`daily_records`",
+      (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
+};
+
+// Fetch daily_record by ID
+const getDaily_RecordById = (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM `ai-journal`.`daily_records` WHERE id = ?",
+      [id],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
+};
+
+module.exports = {
+    getAllDaily_Records,
+    getDaily_RecordById,
+};
