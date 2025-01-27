27545b715d214ff00d5fee12f5c925ab31aa7257
Austin Bray <93416177+AustinBray77@users.noreply.github.com>
2025-01-26 19:36:47 -0500

    Future profile photo functions

--- frontend/app/(main)/settings/page.js
+++ frontend/app/(main)/settings/page.js
@@ -4,11 +4,33 @@ import { logout, useAuthentication } from '@/hooks/authentication';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
+async function getProfilePhoto(user) {
+  await fetch(
+    'https://admin.googleapis.com/admin/directory/v1/users/' + user.email + '/photos/thumbnail',
+    {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + user.id
+      }
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((user_photo) => {
+      document.getElementById('profile-photo').src = 'data:image/png;base64,' + user_photo;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 function ProfilePhoto(props) {
   return (
     <div className="flex justify-center m-5">
       <img
         src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+        id="profile-photo"
         style={{ width: '35vw', borderRadius: '35vw' }}
       />
     </div>
@@ -38,6 +60,9 @@ export default function SettingsPage() {
   if (user == undefined) {
     return <h1>Loading...</h1>;
   }
+
+  //For future implementation with profile photos
+  //getProfilePhoto(user);
 
   return (
     <div className="w-screen">
