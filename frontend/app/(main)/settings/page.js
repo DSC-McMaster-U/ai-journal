7076b1f589513fcd5ad3eb08c607dfd8e278e023
abedmohammed <abedmohammed353@gmail.com>
2025-01-28 11:07:57 -0500

    refactored middleware into layout and updated modal

--- frontend/app/(main)/settings/page.js
+++ frontend/app/(main)/settings/page.js
@@ -3,70 +3,14 @@ import { Button } from '@/components/ui/button';
 import { logout, useAuthentication } from '@/hooks/authentication';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogTrigger,
-  AlertDialogTitle,
-  AlertDialogHeader,
-  AlertDialogAction,
-  AlertDialogFooter
-} from '@/components/ui/alert-dialog';
-
-async function getProfilePhoto(user) {
-  await fetch(
-    'https://admin.googleapis.com/admin/directory/v1/users/' + user.email + '/photos/thumbnail',
-    {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + user.id
-      }
-    }
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((user_photo) => {
-      document.getElementById('profile-photo').src = 'data:image/png;base64,' + user_photo;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
-function ProfilePhoto(props) {
-  return (
-    <div className="flex justify-center m-5">
-      <img
-        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
-        id="profile-photo"
-        style={{ width: '35vw', borderRadius: '35vw' }}
-      />
-    </div>
-  );
-}
-
-function LogoutDialog(props) {
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button onClick={logout}>Logout</Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>You are now logged out.</AlertDialogTitle>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogAction onClick={props.onConfirm}>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
-}
+import Modal from '@/components/common/Modal';
+import { DialogFooter } from '@/components/ui/dialog';
+import Image from 'next/image';
 
 export default function SettingsPage() {
   const router = useRouter();
   const [user, setUser] = useState(undefined);
+  const [open, setOpen] = useState(false);
 
   useAuthentication(
     (user) => {
@@ -77,29 +21,83 @@ export default function SettingsPage() {
     }
   );
 
-  const onLogoutConfirm = () => {
-    router.push('/login');
-  };
-
-  console.log(user);
-
-  if (user == undefined) {
-    return <h1>Loading...</h1>;
+  async function getProfilePhoto(user) {
+    await fetch(
+      'https://admin.googleapis.com/admin/directory/v1/users/' + user.email + '/photos/thumbnail',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + user.id
+        }
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((user_photo) => {
+        document.getElementById('profile-photo').src = 'data:image/png;base64,' + user_photo;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   //For future implementation with profile photos
   //getProfilePhoto(user);
 
+  const onLogoutConfirm = () => {
+    router.push('/login');
+  };
+
+  if (user == undefined) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div className="w-screen">
-      <ProfilePhoto />
+    <div>
+      <h1 className="p-4 text-2xl capitalize border-b-[6px]">Settings</h1>
+
+      <div className="flex justify-center m-5">
+        <Image
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+          id="profile-photo"
+          className="w-[25vw] rounded-full"
+          width={200}
+          height={200}
+          alt="Profile Photo"
+        />
+      </div>
       <div className="flex justify-evenly m-5 text-2xl">
-        {' '}
-        <h1>{user.name}</h1>{' '}
+        <h1>{user.name}</h1>
       </div>
       <h1 className="flex justify-center m-5 text-xl">{user.email}</h1>
       <div className="flex justify-center m-5 text-xl">
-        <LogoutDialog onConfirm={onLogoutConfirm} />
+        <Modal
+          open={open}
+          setOpen={setOpen}
+          title="Logout"
+          trigger={<Button>Logout</Button>}
+          description={'Are you sure you want to logout?'}>
+          <DialogFooter>
+            <div className="flex gap-2 mt-6">
+              <Button
+                className="flex-1"
+                type="button"
+                variant="secondary"
+                onClick={() => setOpen(false)}>
+                Cancel
+              </Button>
+              <Button
+                className="flex-1"
+                onClick={() => {
+                  logout();
+                  onLogoutConfirm();
+                }}>
+                Logout
+              </Button>
+            </div>
+          </DialogFooter>
+        </Modal>
       </div>
     </div>
   );
