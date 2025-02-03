a3dc1e9a9ee5559bcec099da127426586fc8f583
abedmohammed <abedmohammed353@gmail.com>
2025-02-03 18:55:47 -0500

    adding chats page

--- backend/src/controllers/chatsController.js
+++ backend/src/controllers/chatsController.js
@@ -1,56 +1,51 @@
-const chatsService = require('../services/chatsService')
+const chatsService = require('../services/chatsService');
 
 const getChats = async (req, res) => {
   try {
     const userId = req.token.user.id;
-    const chats = await chatsService.getChats(userId)
+    const chats = await chatsService.getChats(userId);
 
     res.status(200).json({ data: chats });
   } catch (error) {
     console.log(`Controller Error: ${error.message}`);
     res.status(500).json({ error: 'Failed to fetch chats' });
   }
-}
+};
 
 const createChat = async (req, res) => {
   try {
-    const userid = req.token.user.id
+    const userId = req.token.user.id;
     const { chatName = null } = req.body;
-    
-    const response = await chatsService.createChat(userId, chatName)
-    res.status(200).json({ data: response });
 
+    const response = await chatsService.createChat(userId, chatName);
+    res.status(200).json({ data: response });
   } catch (error) {
     console.log(`controller error: ${error.message}`);
     res.status(500).json({ error: 'failed to create chat' });
-    
   }
-
-}
+};
 
 const editChat = async (req, res) => {
   try {
-    const { id } = req.params
-    const { chatName, favourited } = req.body
-    const response = await chatsService.editChat(id, chatName, favourited)
-    res.status(200).json({ data: response })
-
+    const { id } = req.params;
+    const { chatName, favourited } = req.body;
+    const response = await chatsService.editChat(id, chatName, favourited);
+    res.status(200).json({ data: response });
   } catch (error) {
     console.log(`controller error: ${error.message}`);
     res.status(500).json({ error: 'failed to edit chat' });
   }
-}
+};
 
 const deleteChat = async (req, res) => {
   try {
-    const { id } = req.params
-    const response = await chatsService.deleteChat(id)
-    res.status(200).json({ data: response})
-
+    const { id } = req.params;
+    const response = await chatsService.deleteChat(id);
+    res.status(200).json({ data: response });
   } catch (error) {
     console.log(`controller error: ${error.message}`);
     res.status(500).json({ error: 'failed to delete chat' });
   }
-}
- 
-module.exports = { getChats, createChat, editChat, deleteChat }
+};
+
+module.exports = { getChats, createChat, editChat, deleteChat };
--- frontend/app/(main)/chats/page.js
+++ frontend/app/(main)/chats/page.js
@@ -1,3 +1,58 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { customFetch } from '@/lib/customFetch';
+import { useEffect, useState } from 'react';
+
 export default function ChatsPage() {
-  return <div>ChatsPage</div>;
+  const [chats, setChats] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getAllInstances();
+  }, []);
+
+  const getAllInstances = async () => {
+    try {
+      setIsLoading(true);
+      const response = await customFetch('/chats', { method: 'GET' });
+      const result = await response.json();
+
+      if (result.error) {
+        throw new Error(result.error);
+      }
+
+      setChats(result.data);
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const createInstance = async () => {
+    try {
+      const response = await customFetch('/chats', {
+        method: 'POST',
+        body: JSON.stringify({ name: 'Espressly AI Bot' })
+      });
+      const result = await response.json();
+
+      if (result.error) {
+        throw new Error(result.error);
+      }
+
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="p-4 text-2xl capitalize border-b-[6px]">Your Messages</h1>
+      <Button onClick={createInstance}>Create Chat</Button>
+
+      <div>{!isLoading && chats.map((chatInstance) => <div>test</div>)}</div>
+    </div>
+  );
 }
