51b02c1866a6a5e6cbd65f68972c643b7f88f3ec
deoignition <93416177+AustinBray77@users.noreply.github.com>
2025-03-18 13:16:16 -0400

    Small changes to logging

--- backend/src/controllers/chatsController.js
+++ backend/src/controllers/chatsController.js
@@ -1,4 +1,5 @@
 const chatsService = require('../services/chatsService');
+const { log, error } = require('../logger');
 
 const getChats = async (req, res) => {
   try {
@@ -7,7 +8,7 @@ const getChats = async (req, res) => {
 
     res.status(200).json({ data: chats });
   } catch (error) {
-    console.log(`Controller Error: ${error.message}`);
+    error(`Controller Error: ${error.message}`);
     res.status(500).json({ error: 'Failed to fetch chats' });
   }
 };
@@ -20,30 +21,34 @@ const createChat = async (req, res) => {
     const response = await chatsService.createChat(userId, chatName);
     res.status(200).json({ data: response });
   } catch (error) {
-    console.log(`controller error: ${error.message}`);
+    error(`Controller error: ${error.message}`);
     res.status(500).json({ error: 'failed to create chat' });
   }
 };
 
 const editChat = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { instanceId } = req.params;
     const { chatName, favourited } = req.body;
-    const response = await chatsService.editChat(id, chatName, favourited);
+    const response = await chatsService.editChat(
+      instanceId,
+      chatName,
+      favourited
+    );
     res.status(200).json({ data: response });
   } catch (error) {
-    console.log(`controller error: ${error.message}`);
+    error(`Controller error: ${error.message}`);
     res.status(500).json({ error: 'failed to edit chat' });
   }
 };
 
 const deleteChat = async (req, res) => {
   try {
-    const { id } = req.params;
-    const response = await chatsService.deleteChat(id);
+    const { instanceId } = req.params;
+    const response = await chatsService.deleteChat(instanceId);
     res.status(200).json({ data: response });
   } catch (error) {
-    console.log(`controller error: ${error.message}`);
+    error(`Controller error: ${error.message}`);
     res.status(500).json({ error: 'failed to delete chat' });
   }
 };
